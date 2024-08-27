959a56f1075f03b1837e974ab794d6ce240567de
Austin Nguyen <austinnguyennn@gmail.com>
2024-08-26 23:08:26 -0500

    fixed cors allowed origins

--- backend/myproject/settings.py
+++ backend/myproject/settings.py
@@ -136,7 +136,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "https://musicreportfrontend.vercel.app/musicreport",# Your React app's URL
+    "https://musicreportfrontend.vercel.app",# Your React app's URL
     # Add other origins if necessary
 ]
 
